5602beff96dd26619dac784dd4b59ba7f8849ca8
RamosArthur1212 <arthur.ramos9@etec.sp.gov.br>
2025-06-09 19:46:33 -0300

    Atualizar o servicodb.php

--- config/servicodb.php
+++ config/servicodb.php
@@ -12,11 +12,11 @@ try{
 //Aqui podem dar continuidade às inserts de serviço
 $cmd = $pdo->query("INSERT INTO servico (id, id_funcionario, id_cliente, id_produto, nome, 
 descricao, preco, pecas_utilizadas, tempo estimado) VALUES
-(1, 1, 3, 2, 'Concerto', 'Em Andamento', 200, '', 'Sete Dias'),
-(2, 3, 1, 3, 'Concerto', 'Finalizado', 200, '', 'Zero Dias'),
-(3, 3, 2, 2, 'Concerto', 'Finalizado', 250, '', 'Zero Dias'),
-(4, 2, 1, 4, 'Concerto', 'Em Andamento', 100, '', 'Quatorze Dias'),
-(5, 2, 3, 4, 'Concerto', 'Finalizado', 200, '', 'Um dia');");
+(1, 1, 3, 2, 'Conserto', 'Em Andamento', 200, '', 'Sete Dias'),
+(2, 3, 1, 3, 'Conserto', 'Finalizado', 200, '', 'Zero Dias'),
+(3, 3, 2, 2, 'Conserto', 'Finalizado', 250, '', 'Zero Dias'),
+(4, 2, 1, 4, 'Troca', 'Em Andamento', 100, '', 'Quatorze Dias'),
+(5, 2, 3, 4, 'Troca', 'Finalizado', 200, '', 'Um dia');");
 
 //Aqui dêem inicio ao Select de uma coluna dessa tabela, pode ser qualquer uma
 
