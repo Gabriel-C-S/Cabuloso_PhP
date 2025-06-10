0ca82362c9cd2c07002d71f9be747026db704eac
Alisson Ruan <alisson.gomes12@etec.sp.gov.br>
2025-06-09 21:24:50 -0300

    Atualizando Banco e Inserindo Dados

--- config/produtodb.php
+++ config/produtodb.php
@@ -9,22 +9,20 @@ try{
     echo "Não foi possível conectar ao banco";
 }
 
-//Aqui podem dar continuidade às inserts de serviço
-$cmd = $pdo->query("INSERT INTO servico (id, id_funcionario, id_cliente, id_produto, nome, 
-descricao, preco, pecas_utilizadas, tempo estimado) VALUES
-(1, 1, 3, 2, 'Conserto', 'Em Andamento', 200, '', 'Sete Dias'),
-(2, 3, 1, 3, 'Conserto', 'Finalizado', 200, '', 'Zero Dias'),
-(3, 3, 2, 2, 'Conserto', 'Finalizado', 250, '', 'Zero Dias'),
-(4, 2, 1, 4, 'Troca', 'Em Andamento', 100, '', 'Quatorze Dias'),
-(5, 2, 3, 4, 'Troca', 'Finalizado', 200, '', 'Um dia');");
+$insert = $pdo->query("INSERT INTO produto (nome) VALUES
+$cmd = $pdo->query("INSERT INTO produto (nome) VALUES
+(1, 'Xbox 360'),
+(2, 'Playsation 5'),
+(3, 'PC Ryzen'),
+(4, 'FIFA 15'),
+(5, 'Notebook Lenovo')");
 
-//Aqui dêem inicio ao Select de uma coluna dessa tabela, pode ser qualquer uma
-
-$cmd = $pdo->prepare("SELECT * FROM servico WHERE id='1'");
-$cmd->bindValues(':id',1);
+$cmd = $pdo->prepare("SELECT * FROM produto WHERE id='2'");
+$cmd->bindValues(':id',4);
 $cmd->execute();
-$resultado=$cmd->fetch(PDO::FETCH_ASSOC);
+$resutado=$cmd->fetch(PDO::FETCH_ASSOC);
 
-foreach($resultado as $key => $value){
+foreach ($resultado as $key => $value) {
     echo $key.":".$value."<br>";
 }
+?>
