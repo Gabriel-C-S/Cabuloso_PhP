314228e523255f7906301ee9c249ff2b4ccea060
RamosArthur1212 <arthur.ramos9@etec.sp.gov.br>
2025-06-16 20:22:39 -0300

    Update clientedb.php

--- config/clientedb.php
+++ config/clientedb.php
@@ -6,27 +6,29 @@ $host = 'localhost';
 $username = 'root';
 
 try{
-    $pdo = new PDO ("mysql:database=$database;host=$host");
+    $pdo = new PDO("mysql:host=$host;dbname=$database", $username, '');
 } catch(Exception $e){
     echo "Não foi possível conectar ao banco";
 }
 
 $insert = $pdo->query ("INSERT INTO cliente (id, nome, telefone, endereco, rg, cpf, email) VALUES
-(1 'Joaquin', '00000000', 'Avenida Paulista', '4234231', '22342134', 'joaquin@gmail.com'), 
-(2 'Jefferson', '11111111', 'José Bonifacio', '12343323', '3421341', 'jefferson@gmail.com'),
-(3 'Pedro', '22222222', 'Antonio Silvio Cunha Bueno', '123123', '2341324', 'pedro@gmail.com'),
-(4 'Paula', '33333333', 'Rua dos Botocudos', '24234213', '2342134', 'paula@gmail.com'),
-(5 'Maria', '44444444', 'Avenida Rio de Janeiro', '223432', '24234', 'maria@gmail.com')");
+(1, 'Joaquin', '00000000', 'Avenida Paulista', '4234231', '22342134', 'joaquin@gmail.com'), 
+(2, 'Jefferson', '11111111', 'José Bonifacio', '12343323', '3421341', 'jefferson@gmail.com'),
+(3, 'Pedro', '22222222', 'Antonio Silvio Cunha Bueno', '123123', '2341324', 'pedro@gmail.com'),
+(4, 'Paula', '33333333', 'Rua dos Botocudos', '24234213', '2342134', 'paula@gmail.com'),
+(5, 'Maria', '44444444', 'Avenida Rio de Janeiro', '223432', '24234', 'maria@gmail.com')");
 
 //Seleção de uma coluna específica de uma tabela através do id e executando por meio do foreach
 
-$insert = $pdo->prepare("SELECT * FROM cliente WHERE id='2'");
-$insert->bindValues(':id',2);
-$insert->execute();
-$insert=$cmd->fetch(PDO::FETCH_ASSOC);
+$cmd = $pdo->prepare("SELECT * FROM cliente WHERE id = :id");
+$cmd->bindValue(':id', 2);
+$cmd->execute();
 
-foreach($resultado as $key => $value){
-    echo $key.":".$value."<br>";
+$resultado = $cmd->fetch(PDO::FETCH_ASSOC);
+
+foreach ($resultado as $key => $value) {
+    echo $key . ": " . $value . "<br>";
 }
+
 
 ?>
